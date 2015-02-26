fe891f51a9b33edefd9481fecd6d0d424721aee6
Luis <lpsilvestrin@inf.ufrgs.br>
2015-02-26 16:25:46 -0300

    fazendo a tab das requests

--- sfcRequestForm.js
+++ sfcRequestForm.js
@@ -1,4 +1,6 @@
+var sfcRequests = [];
 function addNode() {
+  
   var newId = $('#nodesContainer').children().length;
   if($('#nodesContainer').children().length > 0) {
     var lastChild = $('#nodesContainer').children().last();
@@ -16,12 +18,14 @@ function addNode() {
   });
   var radioOption = $("<input/>", {
     type:"radio",
-    name:"nodeType"+newId
+    name:"nodeType"+newId,
+    value:"virtualnode"
   }).click(function() {
     $("#nwfunctionOptions"+newId).hide();
     $("#endpointOptions"+newId).show();
   });
   var radioOption2 = radioOption.clone()
+    .attr("value", "nwfunction")
     .click(function() {
       $("#nwfunctionOptions"+newId).show();
       $("#endpointOptions"+newId).hide();
@@ -30,12 +34,16 @@ function addNode() {
   var radio = $("<div/>", {class:"radio"});
   radio.append($("<label/>", {html:"Virtual node "}).append(radioOption));
   radio.append($("<label/>", {html:"Network function "}).append(radioOption2.attr("checked","true")));
-  var nwfunctionOptions = $("<div/>", {id:"nwfunctionOptions"+newId})
+  var nwfunctionOptions = $("<div/>", {
+    id:"nwfunctionOptions"+newId,
+    class:"nwfunctionopt"})
     .append(input.clone().attr("name","type").attr("placeholder","type"))
     .append(input.clone().attr("name","cpu").attr("placeholder","cpu"))
     .append(input.clone().attr("name","memory").attr("placeholder","memory"));
 
-  var endpointOptions =  $("<div/>", {id:"endpointOptions"+newId})
+  var endpointOptions =  $("<div/>", {
+    id:"endpointOptions"+newId,
+    class:"endpointopt"})
     .append(input.clone().attr("name","position").attr("placeholder","position"))
     .hide();
 
@@ -66,11 +74,12 @@ function addLink() {
     .appendTo("#linksContainer");
 
 }
+
 function addInputs() {
   var nodesContainer = document.getElementById("nodesContainer");
   var linksContainer = document.getElementById("linksContainer");
-  var nodes = document.getElementById("numNodes").value;
-  var links = document.getElementById("numLinks").value;
+  var nodes = document.getElementById("req-numNodes").value;
+  var links = document.getElementById("req-numLinks").value;
   // removing forms generated previously
   while (nodesContainer.hasChildNodes()) {
       nodesContainer.removeChild(nodesContainer.lastChild);
@@ -86,4 +95,60 @@ function addInputs() {
     addLink();
   }
   
+}
+
+function saveRequest() {
+  var links = [];
+  var nodes = [];
+  var tmp;
+  $("#nodesContainer").children().each(function() {
+    if( $(this > '.radio' > 'label' > 'input:checked').val() == "virtualnode" ) {
+      var position = $(this > '#endpointopt' > 'input[name]=position').val();
+      tmp = {type: 'end-point', position: position};
+    } else {
+      var nwType = $(this > '#nwfunctionopt' > 'input[name]=type').val();
+      var cpu = $(this > '#nwfunctionopt' > 'input[name]=cpu').val();
+      var memory = $(this > '#nwfunctionopt' > 'input[name]=memory').val();
+      tmp = {type: 'nwfunction', nwType: nwType, cpu: cpu, memory: memory};
+    }
+    nodes.push(tmp);
+  });
+  $("#linksContainer").children().each(function() {
+    var src = $(this > 'label[for=link-source]' > '#link-source').val();
+    var tgt = $(this > 'label[for=link-target]' > '#link-target').val();
+    var cap = $(this > 'label[for=link-capacity]' > '#link-capacity').val();
+    var tmp = {source: src, target: tgt, capacity: cap};
+    links.push(tmp);
+  });
+  var nextId = 0;
+  if (sfcRequests.length > 0) {
+    nextId = sfcRequests[sfcRequests.length-1].id + 1;
+  }
+  var req = {id: nextId, nodes: nodes, links: links};
+  sfcRequests.push(req);
+
+  var row = $("<tr/>");
+  row.append($("<td/>").html("Network Function "+nextId));
+  var buttonHtml = "<center><button type='button' class='btn btn-danger btn-xs' id='removeID' onClick='$(this).parent().parent().parent().remove();removeRequest("+nextId+");'>Remove</button>";
+  buttonHtml += "<button type='button' class='btn btn-sm btn-primary' data-toggle='modal' data-target='#largeModal' onClick='updateRequestPanel("+nextId+")'>Update</button></center>";
+  row.append($("<td/>").html(buttonHtml));
+  $('#sfc-req-table').append(row);
+
+}
+
+function updateRequestPanel(id) {
+  var req = sfcRequests.filter(function(d) {return d.id == id})[0];
+  var numNodes = req.nodes.length;
+  var numLinks = req.links.length;
+  $('#req-numLinks').val(numLinks);
+  $('#req-numNodes').val(numNodes);
+
+  addInputs();
+
+}
+
+function removeRequest(id) {
+  var req = sfcRequests.filter(function(d) {return d.id == id})[0];
+  var index = sfcRequests.indexOf(req);
+  sfcRequests.splice(index,1);
 }
