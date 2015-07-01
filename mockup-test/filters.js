6cdcf37f75e8a1a62c5fda1c585c27909b6cf3c2
Luis <lpsilvestrin@inf.ufrgs.br>
2015-07-01 16:56:13 -0300

    filtros separados

--- mockup-test/filters.js
+++ mockup-test/filters.js
@@ -1,16 +1,31 @@
 // create the menu area
-var filterMenu = d3.select("#filtersMenu").append("svg")
+var nwMenu = d3.select("#nwMenu").append("svg")
 		.attr("width", "160px")
     	.attr("height", 0);
-var filterButtons = filterMenu.selectAll("g");
+var	nfMenu = d3.select("#nfMenu").append("svg")
+		.attr("width", "160px")
+    	.attr("height", 0);
+
+// global buttons selections
+var nwButtons = nwMenu.selectAll("g");
+var	nfButtons = nfMenu.selectAll("g");
 
 function generateFilters() {
+
+	function Button(id, type, label) {
+		this.type = type;
+		this.label = label;
+		this.id = id;
+		this. visible = true;
+	}
 
 	var physicalLinks = glinks.selectAll(".link.net-1");
 	var nwFunctionNodes = gnodes.selectAll(".node.nwFunction");
 
-	filterButtons = filterButtons.data([]);
-	filterButtons.exit().remove();
+	nwButtons = nwButtons.data([]);
+	nwButtons.exit().remove();
+	nfButtons = nfButtons.data([]);
+	nfButtons.exit().remove();
 
 	var turnVisibility = function(d, i) {
 		var button = d3.select(this);
@@ -27,52 +42,49 @@ function generateFilters() {
 		}
 		d.visible = !d.visible;
 	}
-	var buttons = [];
+	var netBut = []; // buttons data for the networks
+	var nfBut = []; // buttons data for the network functions
 	requests.forEach(function(d, i) {
-		buttons.push({visible:true, type:"virtual", id:i, label:d.name});
+		netBut.push(new Button(i, "virtual", d.name));
 	});
+	netBut.push(new Button(-1, "physical", "Infrastructure"));
 	var usedNF = [];
 	allocatedFunctions.forEach(function(d) {
 		if(usedNF.indexOf(d.nfid) == -1) {
 			usedNF.push(d.nfid);
 		}
 	});
-	buttons.push({visible:true, type:"physical", id:-1, label:"Infrastructure"});
 	usedNF.forEach(function(d) {
-		buttons.push({visible:true, type:"nwFunction", id:d, label:nwFunctions[d].type});
+		nfBut.push(new Button(d, "nwFunction", nwFunctions[d].type));
 	});
 
-	filterMenu.attr("height", 20*buttons.length + 40);
+	nwMenu.attr("height", 20*netBut.length + 40);
+	nfMenu.attr("height", 20*nfBut.length + 40);
 	
-	filterButtons = filterButtons.data(buttons);
-	filterButtons.enter().append("g")
-		.attr("class", "node");
-	filterButtons.append("path")
+	appendButtons(nwButtons, netBut);
+	appendButtons(nfButtons, nfBut);
+
+	// generate the buttons given a selection and the data
+	function appendButtons(butSelect, data) {
+		butSelect = butSelect.data(data);
+		butSelect.enter().append("g")
+			.attr("class", "node");
+		butSelect.append("path")
 		.attr("d", d3.svg.symbol()
         	.type(nodeSymbol)
           	.size(200));
-	filterButtons.append("text")
-	  .attr("x", 12)
-	  .attr("dy", ".35em")
-	  .text(function(d){return d.label});
-	filterButtons.attr("transform", function(d, i) { return "translate(" + 30 + "," + (10 + 20*i) + ")"; })
-		.on("click", turnVisibility);
+		butSelect.append("text")
+		  .attr("x", 12)
+		  .attr("dy", ".35em")
+		  .text(function(d){return d.label});
+		butSelect.attr("transform", function(d, i) { 
+			return "translate(" + 30 + "," + (10 + 20*i) + ")"; 
+		})
+			.on("click", turnVisibility);
 
-	filterButtons.exit().remove();
-	
-	/*
-	function buttonText(d) {
-		var text;
-		if(d.type == "nwFunction") {
-			text = "Function " + d.id;
-		} else if (d.type == "virtual"){
-			text = d.name;
-		} else {
-			text = "Infrastructure";
-		}
-		return text;
+		butSelect.exit().remove();
 	}
-	*/
+
 	function turnVirtualNet(d, opacity) {
 		var id = d.id;
 		var net = glinks.selectAll(".link.net"+id);
